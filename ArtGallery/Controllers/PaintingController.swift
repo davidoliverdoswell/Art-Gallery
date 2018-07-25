4ca9d7f2d59e928207ab433dc1f4ca51de8b54aa
David Doswell <davidoliverdoswell@icloud.com>
2018-07-25 16:47:00 -0400

    Created Painting.swift and Painting Controller

---
+++ ArtGallery/Controllers/PaintingController.swift
@@ -0,0 +1,32 @@
+//
+//  PaintingController.swift
+//  ArtGallery
+//
+//  Created by David Oliver Doswell on 7/25/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PaintingController: UIViewController {
+    
+    var paintings : [Painting] = []
+    
+    func loadPaintingsFromAssets() {
+        for number in 1...12 {
+            if number > 0 {
+                guard let image = UIImage(named: "Image\(number)") else { return }
+                let painting = Painting(image: image)
+                paintings.append(painting)
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadPaintingsFromAssets()
+    }
+    
+}
+
+
+
---
+++ ArtGallery/Model/Painting.swift
@@ -0,0 +1,21 @@
+//
+//  Painting.swift
+//  ArtGallery
+//
+//  Created by David Oliver Doswell on 7/25/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class Painting {
+    
+    let image: UIImage
+    let isLiked: Bool
+    
+    init(image: UIImage, isLiked: Bool = false) {
+        self.image = image
+        self.isLiked = isLiked
+    }
+
+}
